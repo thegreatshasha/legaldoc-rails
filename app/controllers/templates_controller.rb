5a5854f58b0974af50397362e6b6ad96a437f164
Shashwat Kumar <thegreatshasha@gmail.com>
2015-03-07 01:11:08 +0530

    small hack but working excellently

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,5 +1,6 @@
 class TemplatesController < ApplicationController
   before_action :set_template, only: [:show, :edit, :update, :destroy]
+  skip_before_filter :verify_authenticity_token
 
   # GET /templates
   # GET /templates.json
