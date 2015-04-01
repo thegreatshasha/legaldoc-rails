c9fa2d94c60bd2ab7889394873d1010d0dc92152
Shashwat Kumar <thegreatshasha@gmail.com>
2015-04-01 22:40:40 +0530

    using separate account

--- config/initializers/google.rb
+++ config/initializers/google.rb
@@ -1,12 +1,12 @@
 GOOGLE = Hash.new
 GOOGLE[:client] = Google::APIClient.new
-GOOGLE[:key] = Google::APIClient::KeyUtils.load_from_pkcs12('legaldoc-898a708a6c15.p12', 'notasecret')
+GOOGLE[:key] = Google::APIClient::KeyUtils.load_from_pkcs12('legaldoc-api-ee4c10479dbd.p12', 'notasecret')
 GOOGLE[:client].authorization = Signet::OAuth2::Client.new(
 	 :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
 	 :audience => 'https://accounts.google.com/o/oauth2/token',
 	 :scope => 'https://spreadsheets.google.com/feeds/ https://docs.google.com/feeds/ https://www.googleapis.com/auth/drive https://docs.googleusercontent.com/',
 	 #:issuer => APP_CONFIG[APP_CONFIG['HOST_TYPE']]['Google_Issuer'].to_s,
-	 :issuer => "361235141518-7vottu75vgspoof9ujvimqkn5ttgshpv@developer.gserviceaccount.com",
+	 :issuer => "649681412180-nofjchtq3rla23scp7jf1nr1oev3vq8a@developer.gserviceaccount.com",
 	 :access_type => 'offline' ,
 	 :approval_prompt=>'force',
 	 :grant_type => 'authorization_code',
@@ -15,4 +15,4 @@ GOOGLE[:client].authorization = Signet::OAuth2::Client.new(
 GOOGLE[:client].authorization.fetch_access_token!
 
 GOOGLE[:session] = GoogleDrive.login_with_oauth(GOOGLE[:client].authorization.access_token)
-GOOGLE[:customers_sheet] = GOOGLE[:session].spreadsheet_by_key('1eKFpcf123B8FRq2bzo-_ik85WM73zuXgpF_dZRR2Quc').worksheets[0]
+GOOGLE[:customers_sheet] = GOOGLE[:session].spreadsheet_by_key('1Y-xXa1WXEnai-9nUvl1xRXEaQFzUMXe9r1tQVvZf3Zk').worksheets[0]
