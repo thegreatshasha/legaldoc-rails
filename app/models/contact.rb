d44bf60b5b7f07dddfc8cb2c58a9e538e78b9705
Shashwat Kumar <thegreatshasha@gmail.com>
2015-04-01 22:25:14 +0530

    added async google write

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,24 @@
+require 'google/api_client'
+
 class Contact < ActiveRecord::Base
 	self.inheritance_column = :foo
+
+	after_save :write_to_google_drive
+
+	private
+    
+    def write_to_google_drive
+		Thread.new do
+			sheet = GOOGLE[:customers_sheet]
+			max_rows = sheet.rows.length
+
+			arr = self.attributes.slice("type", "name", "email", "tel", "address").map{|d|d[1]}
+
+			arr.to_enum.with_index(1).each do |a, i|
+				sheet[max_rows+1,i] = a
+			end
+
+			sheet.save()
+		end
+    end
 end
